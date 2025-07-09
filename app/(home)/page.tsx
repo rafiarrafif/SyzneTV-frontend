28d81d9792f76bc3399a6a8e8f2f7ac643eb9601
Rafi Arrafif <rafiarrafif05@gmail.com>
2025-07-09 09:58:01 +0700

    :poop: create hero UI wrapper
    still need fix or downgrade tailwind to v3

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,10 +1,18 @@
+import Button from "./button";
 import { metadata } from "./metadata";
 export { metadata };
 
 import React from "react";
 
 const page = () => {
-  return <div className="text-center text-3xl mt-6">Nounoz TV</div>;
+  return (
+    <main>
+      <div className="text-center text-3xl mt-6">Nounoz TV</div>
+      <div>
+        <Button />
+      </div>
+    </main>
+  );
 };
 
 export default page;
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,12 +1,15 @@
 import "./globals.css";
 import GeistFontProvider from "@/providers/fonts/GeistFontProvider";
+import HeroUIWrapper from "@/providers/HeroUIWrapper";
 import React from "react";
 
 const RootLayout = ({ children }: Readonly<{ children: React.ReactNode }>) => {
   return (
     <html lang="en" className="dark">
       <body>
-        <GeistFontProvider>{children}</GeistFontProvider>
+        <GeistFontProvider>
+          <HeroUIWrapper>{children}</HeroUIWrapper>
+        </GeistFontProvider>
       </body>
     </html>
   );
