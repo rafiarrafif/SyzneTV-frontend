3dad8ef5284d72382a598a89910f6e21fd730093
Rafi Arrafif <rafiarrafif05@gmail.com>
2025-08-15 16:39:40 +0700

    :wrench: create backend connector
    create a connector to communicate with the backend service using the ky module.

---
+++ shared/api/connector.ts
@@ -0,0 +1,11 @@
+"use server";
+
+import ky from "ky";
+
+export const api = ky.create({
+  prefixUrl: process.env.MAIN_BACKEND_API_URL,
+  credentials: "include",
+  headers: {
+    access_token: process.env.MAIN_BACKEND_API_KEY,
+  },
+});
