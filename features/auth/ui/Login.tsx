6290f6bffa648ee8d794e5ca1c208f767c19e904
Rafi Arrafif <rafiarrafif05@gmail.com>
2025-08-19 19:58:29 +0700

    :sparkles: create new login page
    create all UI components on the login page, including the header, login form, and OAuth selection

--- features/auth/ui/Login.tsx
+++ features/auth/ui/Login.tsx
@@ -1,7 +1,42 @@
+"use client";
+
 import React from "react";
+import EmailInput from "./EmailInput";
+import { Divider, Link } from "@heroui/react";
+import { routes } from "@/shared/config/routes";
+import OAuthProviders from "./OAuthProviders";
 
 const Login = () => {
-  return <div>This is login flow</div>;
+  return (
+    <div className="pt-12 max-w-[480px] mx-auto">
+      <div className="text-3xl text-center">Welcome back</div>
+
+      {/* Email form */}
+      <div className="mt-6 px-3">
+        <EmailInput />
+      </div>
+
+      {/* Sign up link */}
+      <p className="text-center text-neutral-300 text-sm font-light mt-5">
+        Don't have an account?{" "}
+        <Link className="text-sm font-medium" href={routes.signup}>
+          Sign Up
+        </Link>
+      </p>
+
+      {/* Divider between email form and third-party login options */}
+      <div className="flex w-full items-center mt-6 px-10">
+        <Divider className="flex-1" />
+        <span className="px-2 text-neutral-500 text-sm">or</span>
+        <Divider className="flex-1" />
+      </div>
+
+      {/* Buttons for third-party login options */}
+      <div className="mt-6 px-4">
+        <OAuthProviders />
+      </div>
+    </div>
+  );
 };
 
 export default Login;
