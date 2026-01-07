21a099b77f24468c19b39e5958ef9bdc26b25242
Rafi Arrafif <rafiarrafif05@gmail.com>
2026-01-07 15:13:56 +0700

    ✨ feat: fetch helper connection to main backend
    create a helper to facilitate data requests to and from the main backend.

---
+++ shared/helper/backendFetch.ts
@@ -0,0 +1,26 @@
+interface BackendResponse<T = unknown> {
+  success: boolean;
+  message: string;
+  data?: T;
+  error?: unknown;
+}
+
+export const backendFetch = async (path: string, options: RequestInit = {}) => {
+  const res = await fetch(`${process.env.BACKEND_ENDPOINT}/${path}`, {
+    ...options,
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${process.env.BACKEND_API_KEY}`,
+      ...options.headers,
+    },
+    cache: "default",
+  });
+
+  const resJson = (await res.json()) as BackendResponse;
+
+  if (!res.ok || !resJson.success) {
+    throw new Error(`Elysia error: ${resJson.error}`);
+  }
+
+  return resJson;
+};
