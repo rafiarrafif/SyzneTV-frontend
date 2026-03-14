8393e6393cc8271a45d154a6343999f7dc2724eb
Rafi Arrafif <rafiarrafif05@gmail.com>
2026-03-14 12:00:00 +0700

    🚧 wip: add rating to card

--- features/home/sections/Recommendation/components/Card.tsx
+++ features/home/sections/Recommendation/components/Card.tsx
@@ -1,9 +1,15 @@
 import { RecommendationAnime } from "@/features/home/actions/getRecommenationAnime";
+import { Icon } from "@iconify/react";
+import { StarOff } from "lucide-react";
 
 const AnimeRecommendationCard = ({ data }: { data: RecommendationAnime }) => {
   return (
     <div>
-      <div className="w-64 h-88 rounded-lg overflow-hidden">
+      <div className="w-64 h-88 rounded-lg overflow-hidden relative">
+        <div className="absolute right-2 top-2 bg-amber-400 text-neutral-950 font-semibold flex gap-0.5 items-center py-1 px-2 rounded">
+          <Icon icon="material-symbols:star-rounded" className="h-auto w-4" />
+          <span className="text-sm tracking-tight">{data.rating ?? "N/A"}</span>
+        </div>
         <img
           className="w-full h-full object-cover"
           src={data.thumbnail_url}
